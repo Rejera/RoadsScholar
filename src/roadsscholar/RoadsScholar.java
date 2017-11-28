4c5e8880b7b4c37571497ade06b0498a99f26673
Jeremy Tiberg <jeremywtiberg@CedarvilleUniversity.edu>
2017-11-28 16:00:23 -0500

    Created the project with basic IO

---
+++ src/roadsscholar/RoadsScholar.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package roadsscholar;
+
+import java.io.File;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Jeremy Tiberg and Nathan Herr
+ * 11/28/2017
+ */
+public class RoadsScholar {
+
+
+    public static void main(String[] args) {
+        try{
+            File in = new File("Fill this in later");
+            Scanner inFile = new Scanner(in);
+            int NumOfIntersections = inFile.nextInt();
+            int NumOfRoads = inFile.nextInt();
+            int NumOfCities = inFile.nextInt();
+            
+        }
+        catch(Exception ex){
+            
+        }
+    }
+    
+}
